0f73de13f8dd80408bfb17bec50d8287401bfde9
Kunal Tyagi <kunaltyagi4906@gmail.com>
2026-04-17 14:39:16 +0530

    Update Eyeblink.py

--- Eyeblink.py
+++ Eyeblink.py
@@ -1,6 +1,9 @@
 import cv2
 import cvzone
-from cvzone.FaceMeshModule import FaceMeshDetector
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh()
 from cvzone.PlotModule import LivePlot
 
 # Initialize camera
